a1bd16ea32f82f37df279771061bf6d73aeb8e61
fis <ybbs.daans@hotmail.com>
2018-03-26 16:12:53 +0800

    Add readme, license, initial matmul_w wrapper.
    * linear_algebra.c (matmul_w): New function.

--- linear_algebra.c
+++ linear_algebra.c
@@ -1,3 +1,21 @@
+/* Wrapper for linear algebra.
+   Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+
+This file is part of arrayfire-scm.
+
+arrayfire-scm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+arrayfire-scm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with arrayfire-scm.  If not, see <http://www.gnu.org/licenses/>.    */
+
 #include "linear_algebra.h"
 
 SCM dot_w(SCM _lhs, SCM _rhs)
@@ -24,6 +42,38 @@ SCM dot_w(SCM _lhs, SCM _rhs)
       scm_throw(af_error, message);
     }
 
+  SCM result = scm_make_foreign_object_1(afarray_type, (af_array)out);
+  return result;
+}
+
+SCM matmul_w(SCM _lhs, SCM _rhs)
+{
+  af_array out = 0;
+  af_array lhs = scm_foreign_object_ref(_lhs, 0);
+  af_array rhs = scm_foreign_object_ref(_rhs, 0);
+  af_err errno = AF_SUCCESS;
+  errno = af_dot(&out, lhs, rhs, AF_MAT_NONE, AF_MAT_NONE);
+  if (errno != AF_SUCCESS)
+    {
+      SCM message;
+      message = scm_from_utf8_string("af_matmul failed.\n");
+      scm_throw(af_error, message);
+    }
+  SCM result = scm_make_foreign_object_1(afarray_type, (af_array)out);
+  return result;
+}
+
+SCM transpose_w(SCM _in)
+{
+  af_array in = scm_foreign_object_ref(_in, 0);
+  af_array out = 0;
+  af_err errno = af_transpose(&out, in, false);
+  if (errno != AF_SUCCESS)
+    {
+      SCM message;
+      message = scm_from_utf8_string("transpose_w failed.\n");
+      scm_throw(af_error, message);
+    }
   SCM result = scm_make_foreign_object_1(afarray_type, (af_array)out);
   return result;
 }
--- linear_algebra.h
+++ linear_algebra.h
@@ -1,6 +1,26 @@
+/* Wrapper for linear algebra.
+   Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+
+This file is part of arrayfire-scm.
+
+arrayfire-scm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+arrayfire-scm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with arrayfire-scm.  If not, see <http://www.gnu.org/licenses/>.    */
+
 #ifndef ARRAYFIRE_SCM_LINEAR_ALGEBRA_H
 #define ARRAYFIRE_SCM_LINEAR_ALGEBRA_H
 #include "arrayfire_scm.h"
 
 SCM dot_w(SCM _lhs, SCM _rhs);
+SCM transpose_w(SCM _in);
+
 #endif
