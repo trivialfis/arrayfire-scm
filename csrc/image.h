3f53b4cffd45ec2d0acc2039032a69f8a17e901e
fis <ybbs.daans@hotmail.com>
2018-03-27 04:51:04 +0800

    Restructure. Add some comments for README.

---
+++ csrc/image.h
@@ -0,0 +1,6 @@
+#ifndef AF_SCM_IMAGE_H
+#define AF_SCM_IMAGE_H
+
+
+
+#endif
--- csrc/mathematic.c
+++ csrc/mathematic.c
@@ -1,31 +1,22 @@
+/* Wrapper mathematical functions.
+   Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+
+This file is part of arrayfire-scm.
+
+arrayfire-scm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+arrayfire-scm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with arrayfire-scm.  If not, see <http://www.gnu.org/licenses/>.    */
+
 #include "mathematic.h"
-
-/* SCM eq_w(SCM _lhs, SCM _rhs, SCM batch_p) */
-/* { */
-/*   char is_batch = 1; */
-/*   if (! scm_is_eq(batch_p, SCM_UNDEFINED) && scm_is_false(batch_p)) */
-/*     { */
-/*       is_batch = 0; */
-/*     } */
-/*   scm_assert_foreign_object_type(afarray_type, _lhs); */
-/*   scm_assert_foreign_object_type(afarray_type, _rhs); */
-
-/*   af_array lhs = scm_foreign_object_ref(_lhs, 0); */
-/*   af_array rhs = scm_foreign_object_ref(_rhs, 0); */
-/*   af_array out = 0; */
-
-/*   af_err errno = AF_SUCCESS; */
-
-/*   errno = af_eq(&out, lhs, rhs, is_batch); */
-/*   if (errno != AF_SUCCESS) */
-/*     { */
-/*       SCM message; */
-/*       message = scm_from_utf8_string("af_eq failed.\n"); */
-/*       scm_throw(af_error, message); */
-/*     } */
-/*   SCM result = scm_make_foreign_object_1(afarray_type, (af_array)out); */
-/*   return result; */
-/* } */
 
 SCM af_logical_op(SCM _lhs, SCM _rhs, SCM batch_p,
 		   af_err(*op)(af_array*, af_array, af_array, bool))
