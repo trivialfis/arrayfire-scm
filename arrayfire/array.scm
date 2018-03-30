802feae4345b4a058b7758b6ca7319f20af2b04f
fis <ybbs.daans@hotmail.com>
2018-03-30 11:59:48 -0400

    Add missing license

--- arrayfire/array.scm
+++ arrayfire/array.scm
@@ -1,3 +1,21 @@
+;;; Initialize arrayfire-scm.
+;;; Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+;;;
+;;; This file is part of arrayfire-scm.
+;;;
+;;; arrayfire-scm is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; arrayfire-scm is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with arrayfire-scm.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (arrayfire array)
   #:use-module (oop goops)
   #:use-module (ice-9 optargs)
